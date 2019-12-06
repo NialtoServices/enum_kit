0cb0d4cf6980571b8c53328cafef79a2550d8947
Toby Herbert <tobyherbert+git@protonmail.com>
2019-12-06 12:04:31 +0000

    Use ActiveSupport.on_load in the Railtie

--- lib/enum_kit/railtie.rb
+++ lib/enum_kit/railtie.rb
@@ -10,36 +10,37 @@ module EnumKit
       end
     end
 
-    initializer 'enum_kit.active_record.patches', after: 'active_record.initialize_database' do
-      require 'enum_kit/active_record_patches/connection_adapters/postgresql/column_methods'
-      require 'enum_kit/active_record_patches/connection_adapters/postgresql/oid/enum'
-      require 'enum_kit/active_record_patches/connection_adapters/postgresql/oid/type_map_initializer'
-      require 'enum_kit/active_record_patches/enum'
-      require 'enum_kit/active_record_patches/enum/enum_type'
-      require 'enum_kit/active_record_patches/validations/pg_enum_validator'
-    end
+    initializer 'enum_kit.active_support.register_active_record_handler', after: 'active_support.set_configs' do
+      ActiveSupport.on_load(:active_record) do
+        # Patches
+        require 'enum_kit/active_record_patches/connection_adapters/postgresql/column_methods'
+        require 'enum_kit/active_record_patches/connection_adapters/postgresql/oid/enum'
+        require 'enum_kit/active_record_patches/connection_adapters/postgresql/oid/type_map_initializer'
+        require 'enum_kit/active_record_patches/enum'
+        require 'enum_kit/active_record_patches/enum/enum_type'
+        require 'enum_kit/active_record_patches/validations/pg_enum_validator'
 
-    initializer 'enum_kit.active_record.extensions', after: 'active_record.initialize_databasee' do
-      %w[
-        ConnectionAdapters::PostgreSQL::ColumnDumper
-        ConnectionAdapters::PostgreSQL::SchemaDumper
-        ConnectionAdapters::PostgreSQLAdapter
-        Migration::CommandRecorder
-        SchemaDumper
-      ].each do |extension|
-        next unless Object.const_defined?("ActiveRecord::#{extension}")
+        # Extensions
+        %w[
+          ConnectionAdapters::PostgreSQL::ColumnDumper
+          ConnectionAdapters::PostgreSQL::SchemaDumper
+          ConnectionAdapters::PostgreSQLAdapter
+          Migration::CommandRecorder
+          SchemaDumper
+        ].each do |extension|
+          next unless Object.const_defined?("ActiveRecord::#{extension}")
 
-        require File.join('enum_kit', 'active_record_extensions', EnumKit.underscore(extension))
+          require File.join('enum_kit', 'active_record_extensions', EnumKit.underscore(extension))
 
-        target_constant    = Object.const_get("ActiveRecord::#{extension}")
-        extension_constant = Object.const_get("EnumKit::ActiveRecordExtensions::#{extension}")
+          target_constant    = Object.const_get("ActiveRecord::#{extension}")
+          extension_constant = Object.const_get("EnumKit::ActiveRecordExtensions::#{extension}")
 
-        target_constant.prepend(extension_constant)
+          target_constant.prepend(extension_constant)
+        end
+
+        # Database Types
+        ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:enum] = { name: 'enum' }
       end
-    end
-
-    initializer 'enum_kit.active_record.postgresql.register_enum_type', after: 'active_record.initialize_database' do
-      ActiveRecord::ConnectionAdapters::PostgreSQLAdapter::NATIVE_DATABASE_TYPES[:enum] = { name: 'enum' }
     end
   end
 end
